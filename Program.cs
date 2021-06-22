37ec7a771d30ba1b6a0d506efc7d8ab858852e31
Andrew Hickman <andrew.hickman@aveva.com>
2021-06-22 13:17:02 +0100

    1.0.113.7

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace SqliteRepro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var connection = new SQLiteConnection($"Data Source=data.sqlite;Version=3;");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
